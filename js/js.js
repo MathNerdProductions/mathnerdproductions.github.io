8501a4f04781eadd445c11969e5aa6513835be17
MathNerdProductions <mnprod.ceo@gmail.com>
2015-08-24 13:02:21 -0600

    Fix

--- js/js.js
+++ js/js.js
@@ -7,7 +7,7 @@ $(function(){
 		$(this).on("ended", function(){
 			
 			$("video#spin").hide();
-			$("video#explode").show();
+			$("video#explode").removeClass("hidden").show();
 			$("video#explode").get(0).play();
 			$("video#explode").delay(500).fadeOut(500, function(){
 				$(this).hide();
